f77feec40492b2eda5b188647231468930c8de72
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-13 19:04:03 +0200

    version independent unit testing

--- test/stylist.test.js
+++ test/stylist.test.js
@@ -1,29 +1,36 @@
-const mocha = require('mocha');
 const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
 
 function perform(test, command) {
+    const expected = path.join(__dirname, `expected-${test}`);
+    if (process.env.MOCHA_REGEN) {
+        console.log('Replacing expected output');
+        fs.rmSync(expected, { recursive: true, force: true });
+        cp.execSync(command + ` -o=${expected}`);
+    }
+
+    const output = path.join(__dirname, 'output');
     try {
-        fs.rmSync(path.join(__dirname, 'output'), { recursive: true, force: true });
-        cp.execSync(command);
+        fs.rmSync(output, { recursive: true, force: true });
+        cp.execSync(command + ` -o=${output}`);
         cp.execSync(`diff -burN ${path.join(__dirname, `expected-${test}`)} ${path.join(__dirname, 'output')}`);
         fs.rmSync(path.join(__dirname, 'output'), { recursive: true, force: true });
     } catch (e) {
-        console.log(command);
+        console.log(command + ` -o=${output}`);
         console.log(`diff -burN ${path.join(__dirname, `expected-${test}`)} ${path.join(__dirname, 'output')}`);
         throw e;
     }
 }
 
 const theme = path.resolve(__dirname, '..', 'index.js');
-const build = 'npx documentation build -o=test/output --f=html --theme=' + theme;
+const build = 'npx documentation build --f=html --theme=' + theme;
 
 describe('React Component with TypeScript', () => {
     const tsxBuild = build + ' --parse-extension=tsx --require-extension=tsx';
     const input = path.join(__dirname, 'data', 'RMap.tsx');
     it('no configuration', () => {
-        perform('React-noconf', `${tsxBuild} ${input}`);
+        perform('React-noconf', `${tsxBuild} -c=${path.join(__dirname, 'React-noconf.yml')} ${input}`);
     });
 
     it('w/externalLinks', () => {
